10f7f7fc1877051807d42ffcd21bfeaf700e7e8d
Rahul Sethuram <rahul@sethuram.com>
2020-12-15 21:56:56 +0100

    TSDX

---
+++ tsdx.config.js
@@ -0,0 +1,12 @@
+const images = require('@rollup/plugin-image');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins = [
+      images({ include: ['**/*.png', '**/*.jpg', '**/*.gif'] }),
+      ...config.plugins,
+    ];
+
+    return config;
+  },
+};
