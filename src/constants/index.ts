7b339c4b47db8153696ffbef08065ec4a4ef51ea
sanchaymittal <sanchaymittal@gmail.com>
2020-12-29 04:15:17 +0530

    rmv: router prod identifier const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,13 +2,8 @@ import { TRANSFER_STATES, TransferStates } from './types';
 
 export const CHAIN_INFO_URL = 'https://chainid.network/chains.json';
 
-const PROD_ROUTER_IDENTIFIER =
-  'vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q';
 
 const PROD_IFRAME_WALLET = 'https://wallet.connext.network';
-
-export const routerPublicIdentifier =
-  process.env.REACT_APP_ROUTER_IDENTIFIER || PROD_ROUTER_IDENTIFIER;
 
 export const iframeSrc = process.env.REACT_APP_IFRAME_SRC || PROD_IFRAME_WALLET;
 
