2b877c6d7bb3411552f1a4d8bc534ca898b67369
sanchaymittal <sanchaymittal@gmail.com>
2021-01-17 11:16:41 +0530

    clean up

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { Typography, Grid, CircularProgress } from '@material-ui/core';
-import styled from 'styled-components';
 
 interface LoadingProps {
   initializing: boolean;
@@ -53,40 +52,3 @@ const Loading: FC<LoadingProps> = props => {
 };
 
 export default Loading;
-
-const Load = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(244, 245, 247, 0.8);
-  z-index: 999;
-`;
-
-const LoadingFadeout = styled(Load)`
-  -moz-animation-name: fadeOut;
-  -webkit-animation-name: fadeOut;
-  -ms-animation-name: fadeOut;
-  animation-name: fadeOut;
-  -moz-animation-duration: 0.5s;
-  -webkit-animation-duration: 0.5s;
-  -ms-animation-duration: 0.5s;
-  animation-duration: 0.5s;
-  -moz-animation-fill-mode: forwards;
-  -webkit-animation-fill-mode: forwards;
-  -ms-animation-fill-mode: forwards;
-  animation-fill-mode: forwards;
-`;
-
-const LoadingCircle = styled.div`
-  height: 96px;
-  width: 96px;
-  border-radius: 64px;
-  background-color: #fcd116;
-  overflow: hidden;
-`;
