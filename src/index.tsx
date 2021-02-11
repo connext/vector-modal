bc00c7fbcf76f671a758a57f6aa89c0fe14f9aa1
sanchaymittal <sanchaymittal@gmail.com>
2021-02-11 16:54:35 +0530

    export modals

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
 import ConnextModal from './components/Modal';
+// Add shims and polyfills
+import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
+
+import { define } from 'hybrids';
+import WebModal from './components/WebModal';
+import Counter from './components/Counter';
+
+// Enable HMR for development
+// if (process.env.NODE_ENV !== "production") module.hot.accept();
+
+// Define imported web component
+define('web-modal', WebModal);
+define('react-counter', Counter);
 
 export { ConnextModal };
