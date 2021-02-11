e69999174f34ffc5d70fc86708c3c999aa6824ed
sanchaymittal <sanchaymittal@gmail.com>
2021-02-11 18:05:37 +0530

    rmv: counter testing code

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,11 @@ import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
 
 import { define } from 'hybrids';
 import WebModal from './components/WebModal';
-import Counter from './components/Counter';
 
 // Enable HMR for development
 // if (process.env.NODE_ENV !== "production") module.hot.accept();
 
 // Define imported web component
 define('web-modal', WebModal);
-define('react-counter', Counter);
 
 export { ConnextModal };
