be54962e0ca99525446d617692c9a9a30facabd4
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-18 15:46:33 +0530

    new: export static component

---
+++ src/components/static/index.tsx
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Footer from './Footer';
+import NetworkBar from './NetworkBar';
+import InputForm from './InputForm';
+
+export { Header, Footer, NetworkBar, InputForm };
