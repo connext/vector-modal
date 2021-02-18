4af2a438a49ad5fc8b5dfabf617b3a780508261a
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-18 15:47:49 +0530

    update: transfer error screen

--- src/components/TransferErrorScreen.tsx
+++ src/components/TransferErrorScreen.tsx
@@ -1,62 +1,22 @@
 import React, { FC } from 'react';
-import {
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  IconButton,
-  Button,
-  Text,
-  Stack,
-  NumberInput,
-  NumberInputField,
-  Box,
-  Divider,
-  Input,
-} from '@chakra-ui/react';
-import { MoreVertical } from 'react-feather';
-import { ArrowBackIcon, WarningTwoIcon } from '@chakra-ui/icons';
-
-import { useFormik } from 'formik';
-const arrow = require('../assets/network_arrow.svg') as string;
-const graphic = require('../assets/graphic.svg') as string;
+import { ModalContent, ModalBody, Button, Text, Stack } from '@chakra-ui/react';
+import { Header, Footer, NetworkBar } from './static';
+import { styleModalContent, graphic } from '../constants';
 
 const TransferErrorScreen: FC = () => {
   return (
     <>
       <ModalContent
         id="modalContent"
-        bg="#F5F5F5"
-        border="2px solid #4D4D4D"
-        boxSizing="border-box"
-        borderRadius="15px"
-        padding="0.5rem"
+        style={styleModalContent}
         backgroundImage={`url(${graphic})`}
-        backgroundRepeat="no-repeat"
       >
-        <ModalHeader>
-          <Box w="100%" display="flex" flexDirection="row">
-            <Stack direction="row" spacing={3} alignItems="center" flex="auto">
-              <WarningTwoIcon />
-              <Text fontSize="2xl" casing="uppercase" flex="auto">
-                transfer error
-              </Text>
-            </Stack>
-            <IconButton
-              aria-label="back"
-              border="none"
-              bg="transparent"
-              icon={<ArrowBackIcon boxSize={6} />}
-            />
-            <IconButton
-              aria-label="back"
-              border="none"
-              bg="transparent"
-              icon={<MoreVertical />}
-            />
-          </Box>
-        </ModalHeader>
-        {/* <ModalCloseButton /> */}
+        <Header
+          title="transfer error"
+          warningIcon={true}
+          backButton={true}
+          moreButton={true}
+        />
 
         <ModalBody>
           <Stack direction="column" spacing={7}>
@@ -77,75 +37,17 @@ const TransferErrorScreen: FC = () => {
               </Stack>
             </Stack>
 
-            <Divider
-              orientation="horizontal"
-              style={{ border: '0.25px solid #666666' }}
+            <NetworkBar
+              fromNetwork="Ethereum Mainnet"
+              fromNetworkAsset="USDC"
+              toNetwork="Matic Mainnet"
+              toNetworkAsset="USDC"
+              receiverAddress="0x000abc"
             />
-
-            <Stack direction="column" spacing={5}>
-              <Box display="flex" justifyContent="space-between">
-                <Box>
-                  <Text fontSize="xs" casing="uppercase">
-                    Ethereum Mainnet
-                  </Text>
-                  <Text fontSize="xs" casing="uppercase">
-                    USDC
-                  </Text>
-                </Box>
-                <img src={arrow} />
-                <Box>
-                  <Text fontSize="xs" casing="uppercase">
-                    Matic Mainnet
-                  </Text>{' '}
-                  <Text fontSize="xs" casing="uppercase">
-                    USDC
-                  </Text>
-                </Box>
-              </Box>
-              <Box display="flex" flexDirection="column">
-                <Text fontSize="xs" casing="uppercase">
-                  Receiver Address
-                </Text>
-                <Box
-                  bg="#DEDEDE"
-                  w="100%"
-                  display="flex"
-                  flexDirection="row"
-                  alignItems="center"
-                  borderRadius="15px"
-                >
-                  <Input
-                    id="address"
-                    name="address"
-                    placeholder="0x000"
-                    inputMode="search"
-                    title="Receiver Address"
-                    // styling
-                    boxShadow="none!important"
-                    border="none"
-                    size="lg"
-                    flex="auto"
-                    // misc
-                    default="0x000"
-                    isReadOnly={true}
-                  />
-                </Box>
-              </Box>
-            </Stack>
           </Stack>
         </ModalBody>
 
-        <ModalFooter justifyContent="center">
-          <Text
-            fontSize="xs"
-            casing="uppercase"
-            align="center"
-            fontStyle="normal"
-            fontWeight="bold"
-          >
-            Powered by Connext
-          </Text>
-        </ModalFooter>
+        <Footer />
       </ModalContent>
     </>
   );
