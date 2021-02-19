3b13a915f0cfbdc9ad740a715480627d7d5ee18d
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-20 02:23:49 +0530

    clean up

--- example/index.tsx
+++ example/index.tsx
@@ -19,11 +19,11 @@ function App() {
         onClose={onClose}
         onReady={params => console.log('MODAL IS READY =======>', params)}
         withdrawalAddress={'0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB'}
-        // injectedProvider={(window as any).ethereum}
-        // // prod config
+        injectedProvider={(window as any).ethereum}
+        // prod config
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
         depositAssetId={'0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa'}
-        depositChainProvider="https://goerli.infura.io/v3/"
+        depositChainProvider="https://goerli.infura.io/v3/c80e8facf4bd4b718db243b8dfc1bdec"
         withdrawAssetId={'0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1'}
         withdrawChainProvider="https://rpc-mumbai.matic.today"
         // local config
