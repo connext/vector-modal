3eecbf2d47784fe38eb80d69228d0c0a1b7a3c17
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-19 19:00:16 +0530

    new: images to export

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,5 +1,7 @@
 export const placeholder = require('../assets/placeholder.svg') as string;
 export const arrow = require('../assets/network_arrow.svg') as string;
+export const success = require('../assets/success.svg') as string;
 export const graphic = require('../assets/graphic.svg') as string;
 export const darkGraphic = require('../assets/dark_graphic.svg') as string;
 export const lightGraphic = require('../assets/light_graphic.svg') as string;
+export const loadingGif = require('../assets/loading.gif') as string;
