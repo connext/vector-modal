853a47f7aae2db2b97f52702710be2dc20c01164
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-20 02:18:25 +0530

    clean up

--- src/components/static/index.tsx
+++ src/components/static/index.tsx
@@ -1,6 +1,5 @@
 import Header from './Header';
 import Footer from './Footer';
 import NetworkBar from './NetworkBar';
-import InputForm from './InputForm';
 
-export { Header, Footer, NetworkBar, InputForm };
+export { Header, Footer, NetworkBar };
