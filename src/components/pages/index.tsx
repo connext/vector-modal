6df69940ef2dfc6521803336807fe06d6aa8ef1c
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-19 19:44:43 +0530

    rename compoent

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -2,21 +2,21 @@ import Email from './Email';
 import Login from './Login';
 import Loading from './Loading';
 import Menu from './Menu';
-import Transfer from './Transfer';
 import Status from './Status';
 import Success from './Success';
-import TransferAddress from './TransferAddress';
 import ErrorTransfer from './ErrorTransfer';
 import ErrorSetup from './ErrorSetup';
+import Swap from './Swap';
+import SwapListener from './SwapListener';
 
 export {
   Email,
   Login,
   Loading,
   Menu,
-  Transfer,
+  Swap,
   Success,
-  TransferAddress,
+  SwapListener,
   ErrorTransfer,
   ErrorSetup,
   Status,
