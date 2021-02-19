3e5f77178ead0614aaba9c94cc0ec7213f41ddb1
Sanchay Mittal <sanchaymittal@gmail.com>
2021-02-19 19:00:03 +0530

    new: theme for new design

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -6,6 +6,7 @@ import {
 } from '@material-ui/core/styles';
 import { purple, blue, green } from '@material-ui/core/colors';
 import CSS from 'csstype';
+import { extendTheme } from '@chakra-ui/react';
 
 export const styleModalContent: CSS.Properties = {
   background: '#F5F5F5',
@@ -16,35 +17,135 @@ export const styleModalContent: CSS.Properties = {
   backgroundRepeat: 'no-repeat',
 };
 
-export const theme = createMuiTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: blue[500],
-    },
-    success: {
-      main: green[500],
+export const theme = extendTheme({
+  styles: {
+    global: {
+      fonts: {
+        // body: 'system-ui, sans-serif',
+        heading: '"Cooper Hewitt", serif',
+        // mono: 'Menlo, monospace',
+      },
+      // styles for the `body`
+      body: {
+        // bg: 'gray.400',
+        // color: 'white',
+      },
+      // styles for the `a`
+      a: {
+        color: 'teal.500',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
     },
   },
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
+  colors: {
+    brand: {
+      100: '#f7fafc',
+      // ...
+      900: '#1a202c',
+    },
   },
-  spacing: 2,
+  components: {
+    Button: {
+      baseStyle: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontFamily: 'Roboto',
+        textTransform: 'capitalize',
+      },
+      // Two sizes: sm and md
+      sizes: {
+        sm: {
+          fontSize: '12px',
+          padding: '16px',
+        },
+        md: {
+          fontSize: '16px',
+          padding: '24px',
+        },
+      },
+      // Two variants: outline and solid
+      variants: {
+        outline: {
+          border: '1.5px solid',
+          boxSizing: 'border-box',
+          borderRadius: '15px',
+          borderColor: '#7B7B7B',
+          background: 'white',
+        },
+      },
+      // The default size and variant values
+      defaultProps: {
+        size: 'md',
+        variant: 'outline',
+      },
+    },
+    FormControl: {
+      baseStyle: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontFamily: 'Roboto',
+        textTransform: 'capitalize',
+      },
+      // Two sizes: sm and md
+      sizes: {
+        sm: {
+          fontSize: '12px',
+        },
+        md: {
+          fontSize: '16px',
+          padding: '24px',
+        },
+      },
+      // Two variants: outline and solid
+      variants: {
+        outline: {
+          border: '1.5px solid',
+          boxSizing: 'border-box',
+          borderRadius: '15px',
+          borderColor: '#7B7B7B',
+          background: 'white',
+        },
+      },
+      // The default size and variant values
+      defaultProps: {
+        size: 'md',
+        variant: 'outline',
+      },
+    },
+  },
 });
+
+// export const theme = createMuiTheme({
+//   palette: {
+//     mode: 'light',
+//     primary: {
+//       main: purple[500],
+//     },
+//     secondary: {
+//       main: blue[500],
+//     },
+//     success: {
+//       main: green[500],
+//     },
+//   },
+//   typography: {
+//     fontFamily: [
+//       '-apple-system',
+//       'BlinkMacSystemFont',
+//       '"Segoe UI"',
+//       'Roboto',
+//       '"Helvetica Neue"',
+//       'Arial',
+//       'sans-serif',
+//       '"Apple Color Emoji"',
+//       '"Segoe UI Emoji"',
+//       '"Segoe UI Symbol"',
+//     ].join(','),
+//   },
+//   spacing: 2,
+// });
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
