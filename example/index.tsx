403450ef555211e679d9926ca0e2d39863f3d8ab
sanchaymittal <sanchaymittal@gmail.com>
2021-02-22 22:40:06 +0530

    add: transferAmount param to example

--- example/index.tsx
+++ example/index.tsx
@@ -20,6 +20,7 @@ function App() {
         onReady={params => console.log('MODAL IS READY =======>', params)}
         withdrawalAddress={'0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB'}
         injectedProvider={(window as any).ethereum}
+        transferAmount="0.001"
         // prod config
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
         depositAssetId={'0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa'}
