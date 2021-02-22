99774b37615b3aa6afecc99f9209186beb73d0fa
sanchaymittal <sanchaymittal@gmail.com>
2021-02-22 22:37:13 +0530

    fix: fonts import

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,9 +1,11 @@
 const images = require('@rollup/plugin-image');
+const font = require('rollup-plugin-font');
 
 module.exports = {
   rollup(config, options) {
     config.plugins = [
-      images({ include: ['**/*.png', '**/*.jpg', '**/*.gif'] }),
+      images({ include: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif'] }),
+      font({ unicode: { include: ['**/*.ttf', '**/*.otf'] } }),
       ...config.plugins,
     ];
     return config;
