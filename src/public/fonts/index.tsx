4425f1390362136eaab8569135a19c2261298b49
sanchaymittal <sanchaymittal@gmail.com>
2021-02-24 18:53:53 +0530

    chg: export strategy for assets

--- src/public/fonts/index.tsx
+++ src/public/fonts/index.tsx
@@ -1,9 +1,30 @@
-export const Roboto700 = require('./roboto/Roboto-Bold.ttf');
-export const Roboto500 = require('./roboto/Roboto-Medium.ttf');
-export const Roboto400 = require('./roboto/Roboto-Regular.ttf');
+// export const Roboto700 = require('./roboto/Roboto-Bold.ttf');
+// export const Roboto500 = require('./roboto/Roboto-Medium.ttf');
+// export const Roboto400 = require('./roboto/Roboto-Regular.ttf');
 
-export const RobotoMono400 = require('./roboto_mono/RobotoMono-Regular.ttf');
+// export const RobotoMono400 = require('./roboto_mono/RobotoMono-Regular.ttf');
 
-export const CooperHewitt700 = require('./cooper-hewitt/CooperHewitt-Semibold.otf');
-export const CooperHewitt500 = require('./cooper-hewitt/CooperHewitt-Medium.otf');
-export const CooperHewitt400 = require('./cooper-hewitt/CooperHewitt-Book.otf');
+// export const CooperHewitt700 = require('./cooper-hewitt/CooperHewitt-Semibold.otf');
+// export const CooperHewitt500 = require('./cooper-hewitt/CooperHewitt-Medium.otf');
+// export const CooperHewitt400 = require('./cooper-hewitt/CooperHewitt-Book.otf');
+
+// @ts-nocheck
+import Roboto400 from './roboto/Roboto-Regular.ttf';
+import Roboto500 from './roboto/Roboto-Medium.ttf';
+import Roboto700 from './roboto/Roboto-Bold.ttf';
+
+import RobotoMono400 from './roboto_mono/RobotoMono-Regular.ttf';
+
+import CooperHewitt700 from './cooper-hewitt/CooperHewitt-Semibold.otf';
+import CooperHewitt500 from './cooper-hewitt/CooperHewitt-Medium.otf';
+import CooperHewitt400 from './cooper-hewitt/CooperHewitt-Book.otf';
+
+export {
+  Roboto400,
+  Roboto500,
+  Roboto700,
+  RobotoMono400,
+  CooperHewitt700,
+  CooperHewitt500,
+  CooperHewitt400,
+};
--- src/public/images/index.tsx
+++ src/public/images/index.tsx
@@ -1,8 +1,26 @@
-export const placeholder = require('./placeholder.svg');
-export const arrow = require('./network_arrow.svg');
-export const success = require('./success.svg');
-export const graphic = require('./graphic.svg');
-export const darkGraphic = require('./dark_graphic.svg');
-export const lightGraphic = require('./light_graphic.svg');
-// @ts-ignore
-export const loadingGif = require('./loading.gif');
+// export const placeholder = require('./placeholder.svg');
+// export const arrow = require('./network_arrow.svg');
+// export const success = require('./success.svg');
+// export const graphic = require('./graphic.svg');
+// export const darkGraphic = require('./dark_graphic.svg');
+// export const lightGraphic = require('./light_graphic.svg');
+// export const loadingGif = require('./loading.gif');
+
+// @ts-nocheck
+import loadingGif from './loading.gif';
+import lightGraphic from './light_graphic.svg';
+import darkGraphic from './dark_graphic.svg';
+import graphic from './graphic.svg';
+import success from './success.svg';
+import arrow from './network_arrow.svg';
+import placeholder from './placeholder.svg';
+
+export {
+  placeholder,
+  arrow,
+  success,
+  graphic,
+  darkGraphic,
+  lightGraphic,
+  loadingGif,
+};
