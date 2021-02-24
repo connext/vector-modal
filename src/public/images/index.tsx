ad0f9b3b161924349e18b32d9b23851f9d8dd170
sanchaymittal <sanchaymittal@gmail.com>
2021-02-24 14:34:51 +0530

    chg: location of assets

---
+++ src/public/images/index.tsx
@@ -0,0 +1,7 @@
+export const placeholder = require('./placeholder.svg');
+export const arrow = require('./network_arrow.svg');
+export const success = require('./success.svg');
+export const graphic = require('./graphic.svg');
+export const darkGraphic = require('./dark_graphic.svg');
+export const lightGraphic = require('./light_graphic.svg');
+export const loadingGif = require('./loading.gif');
