431889027be68b25bee28bb2707974e16054652e
sanchaymittal <sanchaymittal@gmail.com>
2021-02-24 14:35:49 +0530

    chg: dir for image and fonts

---
+++ src/public/fonts/index.tsx
@@ -0,0 +1,9 @@
+export const Roboto700 = require('./roboto/Roboto-Bold.ttf');
+export const Roboto500 = require('./roboto/Roboto-Medium.ttf');
+export const Roboto400 = require('./roboto/Roboto-Regular.ttf');
+
+export const RobotoMono400 = require('./roboto_mono/RobotoMono-Regular.ttf');
+
+export const CooperHewitt700 = require('./cooper-hewitt/CooperHewitt-Semibold.otf');
+export const CooperHewitt500 = require('./cooper-hewitt/CooperHewitt-Medium.otf');
+export const CooperHewitt400 = require('./cooper-hewitt/CooperHewitt-Book.otf');
