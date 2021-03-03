384148104d4982c28c70f1dd2e4fc880f3ac2618
sanchaymittal <sanchaymittal@gmail.com>
2021-03-03 22:55:49 +0530

    cleanup loader

--- src/components/static/index.tsx
+++ src/components/static/index.tsx
@@ -2,6 +2,5 @@ import Header from './Header';
 import Footer from './Footer';
 import NetworkBar from './NetworkBar';
 import Options from './Options';
-import Loader from './Loader';
 
-export { Header, Footer, NetworkBar, Options, Loader };
+export { Header, Footer, NetworkBar, Options };
