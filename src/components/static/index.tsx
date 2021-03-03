e6b62d0637523e599e55ed4ac31a43e6434f1ca1
sanchaymittal <sanchaymittal@gmail.com>
2021-03-03 22:02:04 +0530

    cleanup

--- src/components/static/index.tsx
+++ src/components/static/index.tsx
@@ -3,8 +3,5 @@ import Footer from './Footer';
 import NetworkBar from './NetworkBar';
 import Options from './Options';
 import Loader from './Loader';
-// import ThemeWrapper from './ThemeWrapper';
 
-export * from './Theme';
-export * from './Style';
 export { Header, Footer, NetworkBar, Options, Loader };
