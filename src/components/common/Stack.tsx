26d644afc8ed7425ab058e1883b111fda0fba56f
sanchaymittal <sanchaymittal@gmail.com>
2021-03-03 22:02:47 +0530

    structure new components under comman

---
+++ src/components/common/Stack.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import Box from './Box';
+
+type StackProps = {
+  column?: boolean;
+  spacing?: number;
+  margin?: string;
+  justifyContent?: string;
+  alignItems?: string;
+  colorScheme?: string;
+  marginInlineStart?: string;
+  borderRadius?: string;
+};
+
+export function getStackStyles(column: boolean | undefined, spacing: string) {
+  if (column) {
+    return `
+          margin-top: ${spacing};
+          margin-left: 0;
+          `;
+  } else {
+    return `
+        margin-left: ${spacing};
+        margin-top: 0;
+        `;
+  }
+}
+
+export const Stack = styled(Box)<StackProps>`
+  display: flex;
+  margin: ${props => props.margin};
+  justify-content: ${props => props.justifyContent};
+  align-items: ${props => props.alignItems};
+  margin-inline-start: ${props => props.marginInlineStart || '0px'};
+  flex-direction: ${props => (props.column ? 'column' : 'row')};
+  background-color: ${props => props.colorScheme};
+  border-radius: ${props => props.borderRadius};
+
+  & > * {
+    &:not(:first-child) {
+      ${props =>
+        getStackStyles(props.column, props.theme.space[props.spacing ?? 0])}
+    }
+  }
+`;
---
+++ src/components/common/index.tsx
@@ -0,0 +1,13 @@
+import Box from './Box';
+
+export { Box };
+export * from './Button';
+export * from './Fonts';
+export * from './Icon';
+export * from './Input';
+export * from './Link';
+export * from './Menu';
+export * from './Modal';
+export * from './Stack';
+export * from './Text';
+export * from './Theme';
