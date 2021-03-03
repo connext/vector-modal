279d9d7b0b1d25f8863f30ff29892d2e776d8ab0
sanchaymittal <sanchaymittal@gmail.com>
2021-03-03 15:04:07 +0530

    new: fonts & sizes structure

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1392,6 +1392,52 @@ const ConnextModal: FC<ConnextModalProps> = ({
       80: '20rem',
       96: '24rem',
     },
+    fonts: {
+      heading: 'Cooper Hewitt',
+      body: 'Roboto',
+      mono: 'Roboto Mono',
+    },
+    fontSizes: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
+    },
+    sizes: {
+      lg: {
+        h: 12,
+        minW: 12,
+        fontSize: 'lg',
+        px: 6,
+      },
+      md: {
+        h: 10,
+        minW: 10,
+        fontSize: 'md',
+        px: 4,
+      },
+      sm: {
+        h: 8,
+        minW: 8,
+        fontSize: 'sm',
+        px: 3,
+      },
+      xs: {
+        h: 6,
+        minW: 6,
+        fontSize: 'xs',
+        px: 2,
+      },
+    },
   };
 
   return (
