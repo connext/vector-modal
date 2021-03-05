4d6d51a24e335ccc2716609901fbcab100552d48
sanchaymittal <sanchaymittal@gmail.com>
2021-03-05 18:18:33 +0530

    add: truncate func

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -118,3 +118,11 @@ export const getUserBalance = async (
 
   return userBalance;
 };
+
+export const truncate = (str: string, maxDecimalDigits: number) => {
+  if (str.includes('.')) {
+    const parts = str.split('.');
+    return parts[0] + '.' + parts[1].slice(0, maxDecimalDigits);
+  }
+  return str;
+};
