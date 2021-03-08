9684068b6b264c2cf1f2ee7a1ad29ce964f0de69
sanchaymittal <sanchaymittal@gmail.com>
2021-03-08 22:32:22 +0530

    commnent out export defination

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import ConnextModal from './components/Modal';
 // Add shims and polyfills
-import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-
-import { define } from 'hybrids';
-import WebModal from './components/WebModal';
-
-// Enable HMR for development
-// if (process.env.NODE_ENV !== "production") module.hot.accept();
-
-// Define imported web component
-define('connext-modal', WebModal);
-
+// import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
+import ConnextModal from './components/Modal';
 export { ConnextModal };
+// import { define } from 'hybrids';
+// import WebModal from './components/WebModal';
+
+// // Enable HMR for development
+// // if (process.env.NODE_ENV !== "production") module.hot.accept();
+
+// // Define imported web component
+// define('connext-modal', WebModal);
