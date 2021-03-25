6191a177f8e47517b3f91ef859893268d7295bcf
sanchaymittal <sanchaymittal@gmail.com>
2021-03-25 18:33:47 +0530

    fix: example

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,7 @@
 import 'react-app-polyfill/ie11';
+import 'regenerator-runtime/runtime';
 import * as React from 'react';
-import "./App.css";
+import * as ReactDOM from 'react-dom';
 import { Magic } from 'magic-sdk';
 import { providers } from 'ethers';
 
@@ -50,10 +51,10 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <>
       <div
         onChange={async (event) => {
-          console.log("event: ", event.target);
+          console.log('event: ', event.target);
           setLoginType((event.target as any).value);
           await setLoginProvider((event.target as any).value);
         }}
@@ -66,7 +67,7 @@ function App() {
         type="number"
         defaultChecked
         onChange={(event) => setTransferAmount(event.target.value)}
-      />{" "}
+      />{' '}
       Transfer Amount
       <br />
       <button
@@ -74,7 +75,7 @@ function App() {
           try {
             await handleLoginProvider();
           } catch (e) {
-            console.error("Error logging in: ", e);
+            console.error('Error logging in: ', e);
             return;
           }
           setShowModal(true);
@@ -85,24 +86,21 @@ function App() {
       <ConnextModal
         showModal={showModal}
         onClose={() => setShowModal(false)}
-        onReady={(params) => console.log("MODAL IS READY =======>", params)}
-        onDepositTxCreated={(params) =>
-          console.log("onDepositTxCreated =======>", params)
-        }
-        onFinished={(params) => console.log("On finish ==>", params)}
+        onReady={(params) => console.log('MODAL IS READY =======>', params)}
+        onFinished={(params) => console.log('On finish ==>', params)}
         // onSwap={(params) => {
         //   console.log('onSwap params: ', params);
         // throw new Error(params);
         // }}
         transferAmount={transferAmount}
-        withdrawalAddress={"0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB"}
+        withdrawalAddress={'0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB'}
         loginProvider={loginProvider}
         injectedProvider={loginProvider}
         // prod config
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
-        depositAssetId={"0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa"}
+        depositAssetId={'0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa'}
         depositChainProvider="https://goerli.infura.io/v3/af2f28bdb95d40edb06226a46106f5f9"
-        withdrawAssetId={"0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1"}
+        withdrawAssetId={'0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1'}
         withdrawChainProvider="https://rpc-mumbai.matic.today"
         // local config
         // routerPublicIdentifier="vector8Uz1BdpA9hV5uTm6QUv5jj1PsUyCH8m8ciA94voCzsxVmrBRor"
@@ -111,9 +109,8 @@ function App() {
         // withdrawAssetId={'0x9FBDa871d559710256a2502A2517b794B482Db40'}
         // withdrawChainProvider="http://localhost:8546"
       />
-    </div>
+    </>
   );
 }
 
-export default App;
-
+ReactDOM.render(<App />, document.getElementById('root'));
