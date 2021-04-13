a0c0bc3926d04c0202f470edf967277fa3179fd7
sanchaymittal <sanchaymittal@gmail.com>
2021-04-13 17:03:52 +0530

    ✅ add more test

--- modules/sdk/test/service.spec.ts
+++ modules/sdk/test/service.spec.ts
@@ -1,5 +1,5 @@
 import { BrowserNode, CHAIN_DETAIL, ConnextSdk } from "../src";
-import { utils, providers } from "ethers";
+import { utils, providers, BigNumber } from "ethers";
 import Sinon, { createStubInstance } from "sinon";
 import { AllowedSwap, Result } from "@connext/vector-types";
 import { createTestChannelState, mkPublicIdentifier, expect } from "@connext/vector-utils";
@@ -7,7 +7,6 @@ import * as helpers from "../src/utils/helpers";
 import * as connextUtils from "../src/utils/connext";
 
 import { constants } from "ethers";
-import { describe } from "mocha";
 
 const generateChainDetail = (overrides: Partial<CHAIN_DETAIL> = {}): CHAIN_DETAIL => {
   return {
@@ -36,6 +35,7 @@ let getChainMock: Sinon.SinonStub;
 let connectNodeMock: Sinon.SinonStub;
 let createEvtContainerMock: Sinon.SinonStub;
 let getChannelForChainMock: Sinon.SinonStub;
+let getFeesDebouncedMock: Sinon.SinonStub;
 let browserNodeMock: Sinon.SinonStubbedInstance<BrowserNode>;
 let verifyAndGetRouterSupportsMock: Sinon.SinonStub;
 
@@ -50,6 +50,7 @@ describe("service", () => {
     createEvtContainerMock = Sinon.stub(connextUtils, "createEvtContainer");
     getChannelForChainMock = Sinon.stub(connextUtils, "getChannelForChain");
     verifyAndGetRouterSupportsMock = Sinon.stub(connextUtils, "verifyAndGetRouterSupports");
+    getFeesDebouncedMock = Sinon.stub(connextUtils, "getFeesDebounced");
 
     browserNodeMock.sendIsAliveMessage.resolves(Result.ok({ channelAddress: constants.AddressZero }));
     connext.browserNode = browserNodeMock as any;
@@ -253,6 +254,30 @@ describe("service", () => {
       expect(connext.recipientChain).to.be.deep.eq(receiverChain);
     });
 
+    // ToDo
+    it("should throw an error if createEvtContainer errors", async () => {
+      const errorMessage = "evt container errors";
+      createEvtContainerMock.rejects(new Error(errorMessage));
+
+      try {
+        await connext.setup({
+          loginProvider: {},
+          senderAssetId: senderChain.assetId,
+          senderChainProvider: senderChain.chainProvider,
+          recipientAssetId: receiverChain.assetId,
+          recipientChainProvider: receiverChain.chainProvider,
+          routerPublicIdentifier,
+        });
+      } catch (e) {
+        expect(e.message).to.be.deep.eq(errorMessage);
+        expect(e).to.be.ok;
+      }
+      expect(connext.routerPublicIdentifier).to.be.deep.eq(routerPublicIdentifier);
+      expect(connext.senderChain).to.be.deep.eq(senderChain);
+      expect(connext.recipientChain).to.be.deep.eq(receiverChain);
+      expect(connext.browserNode).to.be.deep.eq(browserNodeMock);
+    });
+
     it("should set up with browser node already part of class", async () => {
       verifyAndGetRouterSupportsMock.resolves({
         fromAssetId: senderChain.assetId,
@@ -282,22 +307,90 @@ describe("service", () => {
       expect(connext.senderChainChannel).to.be.deep.eq(senderChannel.channel);
       expect(connext.recipientChainChannel).to.be.deep.eq(receiverChannel.channel);
     });
-
-    // ToDo
-    it.skip("should throw an error if createEvtContainer errors", async () => {});
-    it.skip("should throw an error if senderChainProvider is undefined", async () => {});
-    it.skip("should throw an error if senderAssetId is undefined", async () => {});
-    it.skip("should throw an error if recipientChainProvider is undefined", async () => {});
-    it.skip("should throw an error if recipientAssetId is undefined", async () => {});
   });
 
   describe("estimateFees", () => {
-    it.skip("should return undefined if transferAmount is undefined", async () => {});
-    it.skip("should trim if transferAmount is alphabets or special characters", async () => {});
-    it.skip("should return helper text 'Transfer amount cannot be 0' if transferAmount is zero", async () => {});
-    it.skip("should return fees if transferAmount is numeric string", async () => {});
-    it.skip("should return helper text 'Not enough amount to pay fees' if transferAmount is lower than fees estimateFees", async () => {});
-    it.skip("should return helper text 'Transfer amount exceeds user balance' if transferAmount is lower than fees userBalance", async () => {});
+    const senderChain = generateChainDetail();
+    const receiverChain = generateChainDetail({ chainId: 12 });
+    const senderChannel = createTestChannelState("create");
+    const receiverChannel = createTestChannelState("create");
+    const ResTransferQuote = {
+      routerIdentifier: routerPublicIdentifier,
+      amount: "1000000000000000000",
+      assetId: senderChain.assetId,
+      chainId: senderChain.chainId,
+      recipient: receiverChannel.channel.bobIdentifier,
+      recipientChainId: receiverChain.chainId,
+      recipientAssetId: receiverChain.assetId,
+      fee: "500000000000000000",
+      expiry: "1618308505485",
+      signature:
+        "0xa36cfa752b57e4f35d9af5c8aab4a78dfc336edfb635850e1df2c99c558604b46d0e8686de9590db6f18a500e36014d00acb31b0f738dab2574180805db64fdc1b",
+    };
+
+    it("should return undefined if transferAmount is undefined", async () => {
+      var res = await connext.estimateFees({ transferAmount: undefined });
+
+      expect(res.error).to.be.undefined;
+      expect(res.senderAmount).to.be.empty;
+      expect(res.recipientAmount).to.be.empty;
+      expect(res.totalFee).to.be.undefined;
+      expect(res.transferQuote).to.be.undefined;
+    });
+
+    it("should error 'Invalid amount' if transferAmount is not numeric", async () => {
+      var res = await connext.estimateFees({ transferAmount: "a1" });
+
+      console.log(res);
+      expect(res.error).to.be.deep.eq("Invalid amount");
+    });
+
+    it("should return helper text 'Transfer amount cannot be 0' if transferAmount is zero", async () => {
+      var res = await connext.estimateFees({ transferAmount: "0" });
+
+      console.log(res);
+      expect(res.error).to.be.deep.eq("Transfer amount cannot be 0");
+      expect(res.senderAmount).to.be.deep.eq("0");
+      expect(res.recipientAmount).to.be.empty;
+      expect(res.totalFee).to.be.undefined;
+      expect(res.transferQuote).to.be.undefined;
+    });
+
+    it("should return fees if transferAmount is numeric string", async () => {
+      getFeesDebouncedMock.resolves({
+        transferQuote: ResTransferQuote,
+        totalFee: BigNumber.from("500000000000000000"),
+        senderAmount: BigNumber.from("1000000000000000000"),
+        recipientAmount: BigNumber.from("500000000000000000"),
+      });
+      var res = await connext.estimateFees({ transferAmount: "1" });
+
+      console.log(res);
+      expect(res.error).to.be.undefined;
+      expect(res.senderAmount).to.be.deep.eq("1");
+      expect(res.recipientAmount).to.be.eq("0.5");
+      expect(res.totalFee).to.be.be.eq("0.5");
+      expect(res.transferQuote).to.be.eq(ResTransferQuote);
+    });
+
+    it("should return helper text 'Not enough amount to pay fees' if transferAmount is lower than fees estimateFees", async () => {
+      getFeesDebouncedMock.resolves({
+        transferQuote: ResTransferQuote,
+        totalFee: BigNumber.from("500000000000000000"),
+        senderAmount: BigNumber.from("100000000000000000"),
+        recipientAmount: BigNumber.from("0"),
+      });
+      var res = await connext.estimateFees({ transferAmount: "0.1" });
+
+      console.log(res);
+      expect(res.error).to.be.eq("Not enough amount to pay fees");
+      expect(res.senderAmount).to.be.deep.eq("0.1");
+      expect(res.recipientAmount).to.be.empty;
+      expect(res.totalFee).to.be.be.eq("0.5");
+      expect(res.transferQuote).to.be.eq(ResTransferQuote);
+    });
+
+    it("should return helper text 'Transfer amount exceeds user balance' if transferAmount is lower than fees userBalance", async () => {});
   });
 
   describe("preTransferCheck", () => {
@@ -307,6 +400,27 @@ describe("service", () => {
   });
 
   describe("deposit", () => {
-    
+    it.skip("should error webProvider is undefined", async () => {});
+    it.skip("should error if init or setup function is not called", async () => {});
+    it.skip("should error if transaction is failed or reverted", async () => {});
+    it.skip("should be able to deposit ERC20", async () => {});
+    it.skip("should be able to deposit ETH", async () => {});
+  });
+
+  describe("transfer", () => {
+    it.skip("should error if init or setup function is not called", async () => {});
+    it.skip("should throw an error if reconcileDeposit errors", async () => {});
+    it.skip("should throw an error if createFromAssetTransfer errors", async () => {});
+    it.skip("should throw an error if resolveToAssetTransfer errors", async () => {});
+  });
+
+  describe("withdraw", () => {
+    it.skip("should throw an error if withdrawToAsset errors", async () => {});
+    it.skip("should error if transaction is failed or reverted", async () => {});
+  });
+
+  describe("crossChainSwap", () => async () => {
+    it.skip("should throw an error if transfer errors", async () => {});
+    it.skip("should throw an error if withdraw errors", async () => {});
   });
 });
