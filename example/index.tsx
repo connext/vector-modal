4676f86e9aaf2b851a5a9845824761b5e6ce33d3
Sanchay Mittal <sanchaymittal@gmail.com>
2021-05-05 21:46:22 +0530

    fix: broken example code

--- example/index.tsx
+++ example/index.tsx
@@ -51,7 +51,7 @@ function App() {
     <>
       <div
         onChange={async event => {
-          console.log("event: ", event.target);
+          // console.log("event: ", event.target);
           setLoginType((event.target as any).value);
           await setLoginProvider((event.target as any).value);
         }}
@@ -87,12 +87,13 @@ function App() {
         transferAmount={transferAmount}
         withdrawalAddress={"0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB"}
         injectedProvider={loginProvider}
+        loginProvider={loginProvider}
         // prod config
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
-        depositAssetId={"0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"}
-        depositChainProvider="https://rpc-mainnet.matic.network"
-        withdrawAssetId={"0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3"}
-        withdrawChainProvider="https://bsc-dataseed1.binance.org"
+        depositAssetId={"0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa"}
+        depositChainProvider="https://goerli.infura.io/v3/af2f28bdb95d40edb06226a46106f5f9"
+        withdrawAssetId={"0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1"}
+        withdrawChainProvider="https://rpc-mumbai.matic.today"
         // local config
         // routerPublicIdentifier="vector8Uz1BdpA9hV5uTm6QUv5jj1PsUyCH8m8ciA94voCzsxVmrBRor"
         // depositAssetId={'0x9FBDa871d559710256a2502A2517b794B482Db40'}
