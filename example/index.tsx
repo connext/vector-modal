d4d38732b26f62cf8a731affbe42fcb2d6378aac
Sanchay Mittal <sanchaymittal@gmail.com>
2021-05-13 22:47:59 +0530

    update: example

--- example/index.tsx
+++ example/index.tsx
@@ -56,9 +56,9 @@ function App() {
           await setLoginProvider((event.target as any).value);
         }}
       >
-        <input type="radio" value="metamask" name="loginType" /> Metamask
+        <input type="radio" value="metamask" name="loginType" defaultChecked /> Metamask
         <input type="radio" value="magic" name="loginType" /> Magic
-        <input type="radio" value="none" name="loginType" defaultChecked /> None
+        <input type="radio" value="none" name="loginType" /> None
       </div>
       <input type="number" defaultChecked onChange={event => setTransferAmount(event.target.value)} /> Transfer Amount
       <br />
@@ -90,10 +90,10 @@ function App() {
         loginProvider={loginProvider}
         // prod config
         routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
-        depositAssetId={"0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa"}
-        depositChainProvider="https://goerli.infura.io/v3/af2f28bdb95d40edb06226a46106f5f9"
-        withdrawAssetId={"0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1"}
-        withdrawChainProvider="https://rpc-mumbai.matic.today"
+        depositAssetId={"0x0000000000000000000000000000000000000000"}
+        depositChainProvider="https://kovan.infura.io/v3/af2f28bdb95d40edb06226a46106f5f9"
+        withdrawAssetId={"0x0000000000000000000000000000000000000000"}
+        withdrawChainProvider="https://rinkeby.infura.io/v3/af2f28bdb95d40edb06226a46106f5f9"
         // local config
         // routerPublicIdentifier="vector8Uz1BdpA9hV5uTm6QUv5jj1PsUyCH8m8ciA94voCzsxVmrBRor"
         // depositAssetId={'0x9FBDa871d559710256a2502A2517b794B482Db40'}
