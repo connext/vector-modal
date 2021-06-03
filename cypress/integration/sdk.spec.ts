e27ba253349c0535196418cb6587cdfdcee67e18
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-03 16:56:43 +0530

    update test

--- cypress/integration/sdk.spec.ts
+++ cypress/integration/sdk.spec.ts
@@ -2,10 +2,6 @@
 
 describe("SDK", () => {
   it("visit local host", () => {
-    cy.exec("yarn clean:app && yarn start:app", { log: true, failOnNonZeroExit: false, timeout: 60000 })
-      .its("code")
-      .should("eq", 0);
-
     cy.visit("http://localhost:1234");
   });
 });
